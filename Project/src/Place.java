095cf80bf51be6c00889a3b9d237818047708277
minennn <132678894+minennn@users.noreply.github.com>
2023-06-09 21:05:13 +0800

    對應mapframe加入我需要的屬性

--- Project/src/Place.java
+++ Project/src/Place.java
@@ -2,10 +2,17 @@ public class Place {
     public double lat;
     public double lng;
     public String name;
+    public int pHLimit;
+    public int pLLimit;
+    public double rate;
+    
 
-    public Place(double lat, double lng, String name) {
+    public Place(double lat, double lng, String name, int phl, int pll,double rate) {
         this.lat = lat;
         this.lng = lng;
         this.name = name;
+        this.pHLimit = phl;
+        this.pLLimit = pll;
+        this.rate = rate;
     }
 }
